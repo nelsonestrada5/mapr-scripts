25d29ed4abbf46691a1c070aace7f9d43fa5d78d
Nelson Estrada <nelsonestrada5@gmail.com>
2015-05-05 14:36:27 -0700

    Merged John & Dmitry's hive-site.xml
    Also added the comment/groupings
    1) Client Configuration = 1 property
    2) MetaStore Configuration = 5 properties
    3) Hive Server Configuration = 3 properties
    4) Misc Configuration (commented out) = 15 properties

--- hive-install.sh
+++ hive-install.sh
@@ -1,4 +1,8 @@
-!/bin/bash
+#!/bin/bash
+# nestrada@maprtech.com 2015-May-1
+# Script to Install and Configure MySQL, Hive, Hive Metastore, and HiveServer2
+# hive-site.xml used below merges the configuration properties from both John Benninghoff's & Dmitry Gomerman' files
+
 # Configure clush groups
 grep '## AUTOGEN-HIVE ##' /etc/clustershell/groups >/dev/null 2>&1
 if [ "$?" != "0" ] ; then
@@ -11,28 +15,27 @@ hs2: mapr2
 EOF
 fi
 
-#install mysql, hive, hive metastore, and hiveserver2
+# Install mysql, hive, hive metastore, and hiveserver2
 clush -g mysql "yum install -y mysql-server"
 clush -g hivemeta "yum install -y mapr-hive  mapr-hivemetastore mysql"
 clush -g hs2 "yum install -y mapr-hiveserver2 mysql"
 
-#initial mysql configuration
+# Initial mysql configuration
 clush -g mysql "/etc/init.d/mysqld start"
-#set mysql root password
+# Set mysql root password
 ROOT_PASSWORD=mapr
 clush -g mysql "mysqladmin -u root password $ROOT_PASSWORD"
 
-#node variables needed for mysql and  hive-site.xml configuration
+# Node variables needed for mysql and hive-site.xml configuration
 MYSQL_NODE=$(nodeset -e @mysql)
 METASTORE_NODE=$(nodeset -e @hivemeta)
 HS2_NODE=$(nodeset -e @hs2)
 ZK_NODES=$(nodeset -S, -e @zk)
 
-#set up mysql database and user
+# Set up mysql database, users, and privileges
 DATABASE=hive
 USER=hive
 PASSWORD=mapr
-
 clush -g mysql mysql -u root -p$ROOT_PASSWORD << EOF
 create database $DATABASE;
 create user '$USER'@'%' identified by '$PASSWORD';
@@ -50,7 +53,7 @@ EOF
 # Link this jar file into the Hive lib directory.
 clush -g mysql "ln -s /opt/mapr/lib/mysql-connector-java-5.1.25-bin.jar /opt/mapr/hive/hive-0.13/lib/mysql-connector-java-5.1.25-bin.jar"
 
-#create or modify the hive-site.xml
+# Create or overwrite the hive-site.xml
 clush -g hivemeta,hs2 "cat - << EOF > /opt/mapr/hive/hive-0.13/conf/hive-site.xml
 <?xml version=\"1.0\"?>
 <?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
@@ -61,9 +64,7 @@ clush -g hivemeta,hs2 "cat - << EOF > /opt/mapr/hive/hive-0.13/conf/hive-site.xm
    The ASF licenses this file to You under the Apache License, Version 2.0
    (the "License"); you may not use this file except in compliance with
    the License.  You may obtain a copy of the License at
-
        http://www.apache.org/licenses/LICENSE-2.0
-
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -72,60 +73,114 @@ clush -g hivemeta,hs2 "cat - << EOF > /opt/mapr/hive/hive-0.13/conf/hive-site.xm
 -->
 
 <configuration>
- <property>
-    <name>javax.jdo.option.ConnectionURL</name>
-    <value>jdbc:mysql://$MYSQL_NODE:3306/hive?createDatabaseIfNotExist=true</value>
-    <description>JDBC connect string for a JDBC metastore</description>
+<!-- Hive client, metastore and server configuration all contained in this config file as of Hive 0.13 -->
+
+
+<!-- Client Configuration ========================  -->
+<property>
+    <name>hive.metastore.uris</name>
+    <value>thrift://$METASTORE_NODE:9083</value>
+    <description>Use blank(no value) to enable local metastore, use a URI to connect to a 'remote'(networked) metastore.</description>
 </property>
 
- <property>
+
+<!-- MetaStore Configuration ========================  -->
+<!-- https://cwiki.apache.org/confluence/display/Hive/Configuration+Properties#ConfigurationProperties-MetaStore -->
+<property>
+    <name>javax.jdo.option.ConnectionURL</name>
+    <value>jdbc:mysql://$MYSQL_NODE:3306/hive?createDatabaseIfNotExist=true</value>
+    <description>JDBC connect string for a JDBC metastore such as MySQL</description>
+</property>
+
+<property>
     <name>javax.jdo.option.ConnectionDriverName</name>
     <value>com.mysql.jdbc.Driver</value>
     <description>Driver class name for a JDBC metastore</description>
- </property>
+</property>
 
- <property>
+<property>
     <name>javax.jdo.option.ConnectionUserName</name>
     <value>$USER</value>
     <description>username to use against metastore database</description>
- </property>
+</property>
 
- <property>
+<property>
     <name>javax.jdo.option.ConnectionPassword</name>
     <value>$PASSWORD</value>
     <description>password to use against metastore database</description>
- </property>
-
- <property>
-    <name>hive.metastore.uris</name>
-    <value>thrift://$METASTORE_NODE:9083</value>
- </property>
-
-<property>
-  <name>hive.support.concurrency</name>
-  <description>Enable Hive's Table Lock Manager Service</description>
-  <value>true</value>
 </property>
 
 <property>
-  <name>hive.zookeeper.quorum</name>
-  <description>Zookeeper quorum used by Hive's Table Lock Manager</description>
-  <value>$ZK_NODES</value>
+    <name>hive.metastore.execute.setugi</name>
+    <value>true</value>
+    <description> Set this property to enable Hive Metastore service impersonation in unsecure mode. In unsecure mode, setting this property to true causes the metastore to execute DFS operations using the client's reported user and group permissions. Note that this property must be set on BOTH the client and server sides. Further note that its best effort. If client sets its to true and server sets it to false, client setting will be ignored. </description>
+</property>
+
+
+<!-- Hive Server Configuration ========================  -->
+<!-- https://cwiki.apache.org/confluence/display/Hive/Configuration+Properties#ConfigurationProperties-HiveServer2 -->
+<property>
+    <name>hive.server2.thrift.port</name>
+    <value>10001</value>
+    <description>TCP port number for Hive Server to listen on, default 10000, conflict with webmin</description>
 </property>
 
 <property>
-  <name>hive.zookeeper.client.port</name>
-  <value>5181</value>
-  <description>The Zookeeper client port. The MapR default clientPort is 5181.</description>
+    <name>hive.server2.enable.impersonation</name>
+    <value>true</value>
+    <description>Set this property to enable impersonation in Hive Server 2, not in above URL?</description>
 </property>
 
 <property>
-  <name>hive.metastore.execute.setugi</name>
-  <value>true</value>
-  <description> Set this property to enable Hive Metastore service impersonation in unsecure mode.
-   In unsecure mode, setting this property to true causes the metastore to execute DFS operations
-   using the client's reported user and group permissions. Note that this property must be set on
-   BOTH the client and server sides. </description>
+    <name>hive.server2.enable.doAs</name>
+    <value>true</value>
+    <description>Set this property to enable impersonation in Hive Server 2</description>
+</property>
+
+
+<!-- Misc Configuration ========================  -->
+<!-- commented out
+<property>
+    <name>hive.support.concurrency</name>
+    <value>true</value>
+    <description>Enable Hive's Table Lock Manager Service, requires zookeeper config</description>
+</property>
+
+<property>
+    <name>hive.zookeeper.quorum</name>
+    <value>$ZK_NODES</value>
+    <description>Zookeeper quorum used by Hive's Table Lock Manager</description>
+</property>
+
+<property>
+    <name>hive.zookeeper.client.port</name>
+    <value>5181</value>
+    <description>The Zookeeper client port. The MapR default clientPort is 5181.</description>
+</property>
+
+<property>
+    <name>datanucleus.autoCreateSchema</name>
+    <value>true</value>
+</property>
+
+<property>
+    <name>datanucleus.autoCreateTables</name>
+    <value>true</value>
+</property>
+
+<property>
+    <name>datanucleus.autoCreateColumns</name>
+    <value>true</value>
+</property>
+
+<property>
+    <name>datanucleus.fixedDatastore</name>
+    <value>false</value>
+</property>
+
+<property>
+    <name>datanucleus.autoStartMechanism</name>
+    <value>SchemaTable</value>
 </property>
 
 <property>
@@ -141,7 +196,7 @@ clush -g hivemeta,hs2 "cat - << EOF > /opt/mapr/hive/hive-0.13/conf/hive-site.xm
 
 <property>
     <name>hive.security.authorization.enabled</name>
-    <name>false</name>
+    <value>false</value>
 </property>
 
 <property>
@@ -160,15 +215,11 @@ clush -g hivemeta,hs2 "cat - << EOF > /opt/mapr/hive/hive-0.13/conf/hive-site.xm
 </property>
 
 <property>
-    <name>hive.server2.enable.doAs</name>
-    <value>true</value>
-</property>
-
-<property>
   <name>hive.optimize.insert.dest.volume</name>
   <value>true</value>
   <description>For CREATE TABLE AS and INSERT queries create the scratch directory under the destination directory. This avoids the data move across volumes and improves performance.</description>
 </property>
+-->
 
 </configuration>
 EOF
